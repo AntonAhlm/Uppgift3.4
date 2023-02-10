e18ed94fdcd5837a9cf694a2f4f4efad2422b472
Anton <>
2023-02-10 08:53:17 +0100

    Add project files.

---
+++ Uppgift3.4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur många **minuter** lång är låten?");
+            int minut = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Hur många **sekunder** lång är låten?"); 
+            int sekund = Convert.ToInt32(Console.ReadLine());
+            int tid = minut * 100 + sekund;
+
+            if (tid >= 245 && tid <= 420)
+                Console.WriteLine("Din låt får spelas");
+            else
+                Console.WriteLine("Din låt får inte spelas");
+
+
+
+        }
+    }
+}
